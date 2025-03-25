ecb40e18de705078904c30e39389c6bb74b50777
zedd-s-repo <levialbehu@gmail.com>
2025-03-25 20:10:52 +0100

    'week_1'

---
+++ work work/play.py
@@ -0,0 +1,27 @@
+# # first_name = "Dumebi"
+# # last_name = "Ojo"
+# # # email = first_name + last_name + "@pau.edu.ng"
+# # # print(email.lower())
+# #
+# # email = f"{first_name}{last_name}@pau.edu.ng".lower()
+# # print(email)
+# # first_name  [2:5]
+# # print(first_name)
+# #
+#
+# message = '2'
+# course_code = 'CSC 102'
+# course_title = 'Introduction To Problem Solving'
+# msg = message + ' ' + course_code + ' ' + course_title
+# print(msg)
+#
+# message0 = int('2')
+# print(message0)
+
+def area_of_rec():
+    l = int(input("Enter Lenghth: "))
+    b = int(input("Enter breadth: "))
+    a = l * b
+    print("Area of rectangle is: ", a)
+
+area_of_rec()
