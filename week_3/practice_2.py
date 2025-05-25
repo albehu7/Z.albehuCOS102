ced283f53d49382820391afbdffb011071e42c01
zedd-s-repo <levialbehu@gmail.com>
2025-05-25 18:36:09 +0100

    Week 3

---
+++ week_3/practice_2.py
@@ -0,0 +1,10 @@
+# To check if a program is Positive, Negative OR 0
+
+num = float(input("Enter number: "))
+if num  > 0:
+    print("Positive number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative Number")
+
